6b09c6e82aaf91de891f7760918d0c619a548ddc
neverm1ndo <it@nmnd.ru>
2023-09-16 16:40:02 +0300

    set pug as view engine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import path from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+        app.useStaticAssets(path.join(__dirname, '..', 'public'));
+        app.setBaseViewsDir(path.join(__dirname, '..', 'views'));
+        app.setViewEngine('pug');
+  
   await app.listen(3000);
 }
 bootstrap();
